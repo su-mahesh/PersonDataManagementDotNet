bf3734dfe3e02147d78ff97c41921bf1a0b511b1
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-11 14:03:02 +0530

    ADDED: PersonCLass

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Person
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ PersonDataManagement/PersonDataManagement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    class PersonDataManagement
+    {
+        public static void AddPersonDetails(List<Person> personList)
+        {
+            personList.Add(new Person() { SSN = 1, Name = "Dipesh", Age = 24, Address = "Mumbai" });
+            personList.Add(new Person() { SSN = 2, Name = "Dhanesh", Age = 24, Address = "Banglore" });
+            personList.Add(new Person() { SSN = 3, Name = "Kaousr", Age = 75, Address = "Pune" });
+            personList.Add(new Person() { SSN = 4, Name = "Moenei", Age = 18, Address = "Chennai" });
+            personList.Add(new Person() { SSN = 5, Name = "Itedite", Age = 24, Address = "Delhi" });
+        }
+        private static void ShowRecords(List<Person> PersonList)
+        {
+            foreach (Person Person in PersonList)
+            {
+                Console.WriteLine(Person.SSN);
+                Console.WriteLine(Person.Name);
+                Console.WriteLine(Person.Age);
+                Console.WriteLine(Person.Address);
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Person> PersonList = new List<Person>();
+            AddPersonDetails(PersonList);
+            ShowRecords(PersonList);
+        }      
+    }
+}
